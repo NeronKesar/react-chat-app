b681878a51c567913ba7f481c8a2e5c3876983e4
Neron_Kesar <nero.kesar@gmail.com>
2018-07-17 09:13:33 +0300

    add project

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import Root from './components/Root';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <Root />
+      </div>
+    );
+  }
+}
+
+export default App;
