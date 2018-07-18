fb2374631655fb5b05917c06ce0a5a8bd46dcf88
Neron_Kesar <nero.kesar@gmail.com>
2018-07-18 17:23:47 +0300

    add Title

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
+import { Router } from 'react-router-dom';
 import Root from './components/Root';
+import history from './history';
 
 class App extends Component {
   render() {
     return (
-      <div>
+      <Router history={history}>
         <Root />
-      </div>
+      </Router>
     );
   }
 }
---
+++ src/config.js
@@ -0,0 +1,13 @@
+import firebase from 'firebase';
+
+export const appName = 'react-chat-app-de99d';
+export const firebaseConfig = {
+  apiKey: "AIzaSyDjcJAgaq0nL2Hr57vqZ4qgaMW2xcQRI_M",
+  authDomain: `${appName}.firebaseapp.com`,
+  databaseURL: `https://${appName}.firebaseio.com`,
+  projectId: appName,
+  storageBucket: "",
+  messagingSenderId: "129089518968"
+};
+
+firebase.initializeApp(firebaseConfig);
