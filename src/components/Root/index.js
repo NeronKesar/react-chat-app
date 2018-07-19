35d59f2cf9d681a8ad8d4e751bd3bfd6e8b6e415
Neron_Kesar <nero.kesar@gmail.com>
2018-07-19 11:26:30 +0300

    add Sign In button

--- src/components/Root/index.js
+++ src/components/Root/index.js
@@ -1,36 +1,45 @@
 import React, { Component } from 'react';
-import { Navbar } from 'react-bootstrap';
+import { Navbar, Nav, NavItem } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 import './index.css'
 
 class Root extends Component {
   render() {
     return (
-      <div>
-        <Navbar className="Navbar">
-          <Navbar.Header>
-            <Navbar.Brand>
-              <Link
-                to="/"
-                style={{ textDecoration: 'none' }}
-                activeStyle={{ color: 'red' }}
-              >
-                <h1 className="Title">CHAT-APP</h1>
-              </Link>
-            </Navbar.Brand>
-            <Navbar.Toggle />
-            <Navbar.Brand>
-              <Link to="/profile">Profile</Link>
-            </Navbar.Brand>
-            <Navbar.Brand>
-              <Link to="/auth">Sing In</Link>
-            </Navbar.Brand>
-          </Navbar.Header>
-        </Navbar>
-        Hello World
+      <div className="Root">
+
+        <Link
+          to="/"
+          style={style}
+        >
+          <h1 className="Title">CHAT-APP</h1>
+        </Link>
+
+        <div className="Navbar">
+
+          <div className="NavbarContainer">
+
+          </div>
+
+          <div className="SingInButton">
+            <Link
+              className="SingInText"
+              to="/auth"
+              style={style}
+            >
+              Sing In
+            </Link>
+          </div>
+
+        </div>
+
       </div>
     )
   }
 }
 
 export default Root;
+
+const style = {
+  textDecoration: 'none'
+};
