cc4d46b73cde2a374323209311849dcd51f94c9a
Neron_Kesar <nero.kesar@gmail.com>
2018-07-30 09:54:53 +0300

    add new forms to sign up form

--- src/components/auth/SignUpForm/index.js
+++ src/components/auth/SignUpForm/index.js
@@ -2,33 +2,63 @@ import React, { Component } from 'react';
 import { reduxForm, Field } from 'redux-form';
 import emailValidator from 'email-validator';
 import InputField from '../../common/InputField';
+import './style.css';
 
 class SignUpForm extends Component {
   render() {
     const { handleSubmit } = this.props;
 
     return (
-      <div>
-        <h2>Sign Up</h2>
-        <form onSubmit={handleSubmit}>
-          <div>
-            <label>Email</label>
+      <div className="SignUpFormRoot">
+
+        <h1 className="SignUpFormTitle">Sign Up</h1>
+
+        <form onSubmit={handleSubmit} className="SignUpFormInputs">
+
+          <div className="SignUpFormInputContainer">
+
+            <label className="SignUpFormLabel">Email</label>
+
             <Field name="email" component={InputField} />
+
           </div>
-          <div>
-            <label>Password</label>
+
+          <div className="SignUpFormInputContainer">
+
+            <label className="SignUpFormLabel">User Name</label>
+
+            <Field name="userName" component={InputField} />
+
+          </div>
+
+          <div className="SignUpFormInputContainer">
+
+            <label className="SignUpFormLabel">Password</label>
+
             <Field name="password" component={InputField} type="password" />
+
           </div>
-          <div>
-            <input type="submit" />
+
+          <div className="SignUpFormInputContainer">
+
+            <label className="SignUpFormLabel">Repeat Password</label>
+
+            <Field name="repeatPassword" component={InputField} type="password" />
+
           </div>
+
+          <div className="SignUpFormSubmitButtonContainer">
+            <button type="submit" className="SignUpFormSubmitButton">Submit</button>
+          </div>
+
         </form>
+
       </div>
     )
   }
 }
 
-const validate = ({ email, password }) => {
+const validate = ({ email, userName, password, repeatPassword }) => {
   const errors = {};
 
   if (!email) {
